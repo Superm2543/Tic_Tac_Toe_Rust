1ec7248bef01078466909f174e8700fb2cc6dcb9
Superm2543 <Superm2543k@gmail.com>
2025-10-03 13:38:05 -0400

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,14 @@
+use eframe::egui::{CentralPanel, Context, FontFamily, FontId, TextStyle, TopBottomPanel};
+use eframe::{egui, Frame};
+#[derive(Default)]
+struct App{
+
+}
+impl eframe::App for App {
+    fn update(&mut self, ctx: &Context, _frame: &mut Frame) {
+        
+    }
+}
+fn main() {
+    println!("Hello, world!");
+}
